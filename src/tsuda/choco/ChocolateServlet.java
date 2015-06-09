cc90b8f0f11d4718393fdcd93e39ee2e7cfe7837
g13913ks <g13913ks@m20214.tsuda.ac.jp>
2015-06-09 14:43:48 +0900

    loginからsiteに飛べるようにしました。

--- src/tsuda/choco/ChocolateServlet.java
+++ src/tsuda/choco/ChocolateServlet.java
@@ -17,6 +17,12 @@ public class ChocolateServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		
+		resp.setContentType("text/plain");
+		resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+        req.setCharacterEncoding("utf-8");
+		
 		UserService userService = UserServiceFactory.getUserService();
 
         String thisURL = req.getRequestURI();
@@ -24,28 +30,33 @@ public class ChocolateServlet extends HttpServlet {
         resp.setContentType("text/html");
 
         if (req.getUserPrincipal() != null) {
+        	resp.getWriter().println("");
             resp.getWriter().println("<p>Welcome, " +
                                      req.getUserPrincipal().getName() +
                                      "!<br>  You can <a href=\"" +
-                                     userService.createLogoutURL(thisURL) +
-                                     "\">sign out</a>.</p>");
+                                     userService.createLogoutURL("/cart.do") +
+                                     "\">go site</a>.</p>");
         } else {
+        	resp.getWriter().println("<p>Welcome to chocolate shop!</p>");
+        	resp.getWriter().println("<p>Googleアカウントでログインしてください</p>");
             resp.getWriter().println("<p>Please <a href=\"" +
                                      userService.createLoginURL(thisURL) +
-                                     "\">sign in</a>.</p>");
+                                     "\">Login</a>.</p>");
         }
 
-        /*
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+        
+		//resp.setContentType("text/plain");
+		//resp.getWriter().println("Hello, world");
 		
-		PersistenceManagerFactory factory = PMF.get();
-        PersistenceManager manager = factory.getPersistenceManager();
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html");
-        req.setCharacterEncoding("utf-8");
-        String param1 = req.getParameter("name");
-        PrintWriter out = resp.getWriter();
+		//PersistenceManagerFactory factory = PMF.get();
+        //PersistenceManager manager = factory.getPersistenceManager();
+        //resp.setCharacterEncoding("UTF-8");
+        //resp.setContentType("text/html");
+       // req.setCharacterEncoding("utf-8");
+       // String param1 = req.getParameter("name");
+       // PrintWriter out = resp.getWriter();
+        
+        /*
         List<LinkData> list = null;
         
         if (param1 == null || param1 ==""){
--- war/ChocoListFromDB.jsp
+++ war/ChocoListFromDB.jsp
@@ -8,18 +8,18 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <div align="center">
+        <div align="left">
             <h1>世界のチョコ屋さん -　商品一覧</h1>
             
             <h2><c:out value="${name}"/>さん、お好きな輸入したいチョコを選んでください。</h2>
             
             <form action="cart.do" method="post">
-               	<input type="checkbox" name="choco" value="choco1">ハワイ<br>
-               	<input type="checkbox" name="choco" value="choco2">アメリカ<br>
-               	<input type="checkbox" name="choco" value="choco3">イタリア<br>
-               	<input type="checkbox" name="choco" value="choco4">オーストラリア<br>
-               	<input type="checkbox" name="choco" value="choco5">スイス<br>
-               	
+               	<input type="checkbox" name="choco" value="choco1">ハワイ　　　　　マカダミアナッツ<br>
+               	<input type="checkbox" name="choco" value="choco2">アメリカ　　　　キスチョコ<br>
+               	<input type="checkbox" name="choco" value="choco3">イタリア　　　　カントチー二チョコ<br>
+               	<input type="checkbox" name="choco" value="choco4">オーストラリア　ティムタムチョコ<br>
+               	<input type="checkbox" name="choco" value="choco5">スイス　　　　　ホワイトアンドゴールドチョコ<br>
+               	<br>
                 <input type="submit" name="cart.do" value="カートの内容を確認して注文へ進む" />
                 
             </form>
